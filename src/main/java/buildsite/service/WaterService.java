963ab9b88621a85f0d4122fa45d5bd856d4e25fa
zhuchengkai1 <109274784+zhuchengkai1@users.noreply.github.com>
2022-07-18 19:12:32 +0800

    Update WaterService.java

--- src/main/java/buildsite/service/WaterService.java
+++ src/main/java/buildsite/service/WaterService.java
@@ -1,5 +1,14 @@
 package buildsite.service;
+import buildsite.model.Water;
+
+import java.util.List;1
+
 
 public interface WaterService {
 
+    Water add(Water water);
+
+    Water get(String id);
+
+    boolean delete(String id);
 }
